a34c1077fe4edff0ec3e6a7d7e698395fd8f0b5d
Hung Nguyen Duc <ndgnuh@protonmail.com>
2024-12-23 03:55:39 +0700

    rebuild

--- src/luac.c
+++ src/luac.c
@@ -397,14 +397,14 @@ int luaC_init(char** library_path)
 
     if (library_path != NULL) {
         // WARNING: this will modify library_path
-        char library_path_i;
+        char* library_path_i;
         int i = 0;
         while (1) {
             library_path_i = *(library_path + i);
             i += 1;
             if (library_path_i == NULL) break;
             cwc_log(CWC_ERROR, "Extra library path %s", library_path_i);
-            add_to_search_path(L, &library_path_i);
+            add_to_search_path(L, library_path_i);
         }
     }
     else {
